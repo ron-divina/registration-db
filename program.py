0a178a5977575c19fdc00f6798a53c82419a1f77
Ron Divina <rondavin.divina@gmail.com>
2020-04-22 00:01:27 +0800

    Add function add_item for option

--- program.py
+++ program.py
@@ -37,7 +37,7 @@ def main_menu():
 	elif menu_input == 2:
 		birthdays_today()
 	elif menu_input == 3:
-		pass
+		add_item()
 	elif menu_input == 4:
 		pass
 	elif menu_input == 5:
@@ -109,6 +109,68 @@ def birthdays_today():
 
 	input('Press any key to continue...')
 	main_menu()
+
+def add_item():
+	confirm_input = ''
+	while confirm_input.upper() != 'Y':
+		valid_id = False
+		while valid_id == False:
+			try:
+				input_id = int(input('Enter valid id (whole number(s) only): '))
+				valid_id = True
+			except ValueError:
+				continue
+
+		is_taken = check_id(input_id)
+		while is_taken:
+			try:
+				input_id = int(input('The ID is already taken. Please try again: '))
+			except ValueError:
+				continue
+			is_taken = check_id(input_id)
+
+		input_first_name = input('Enter first name: ')
+		input_last_name = input('Enter last name: ')
+		input_birthday = input('Enter birthday: ')
+		is_invalid_birthday = check_birthday(input_birthday.capitalize())
+		while is_invalid_birthday:
+			input_birthday = input('Enter valid Birthday (e.g. August 10, 1999): ')
+			is_invalid_birthday = check_birthday(input_birthday.capitalize())
+
+		confirm_input = input(f''' 
+
+    		Check if your inputs are correct:
+    		ID: {input_id}
+    		Full name: {input_first_name.capitalize()} {input_last_name.capitalize()}
+    		Birthday: {input_birthday.capitalize()}
+
+    		(Y/N): ''')
+
+		if confirm_input.upper() == 'Y':
+			db.insert(len(db), 
+					{
+					'id': input_id, 
+					'first_name': input_first_name.capitalize(), 
+					'last_name': input_last_name.capitalize(),
+					'birthday': input_birthday
+					 })
+			input('Record added. Press any key to continue...')
+			main_menu()
+
+def check_id(id):
+	for item in db:
+		if item['id'] == id:
+			return True
+	return False 
+
+def check_birthday(birthday):
+	from datetime import datetime
+	try:
+		if birthday != datetime.strptime(birthday, '%B %d, %Y').strftime('%B %d, %Y'):
+			raise ValueError
+		return False
+	except ValueError:
+		return True
 
 main_menu()
 
