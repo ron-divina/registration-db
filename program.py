0ed74dfa7f0841e01736e07140915082cd90968e
Ron Divina <rondavin.divina@gmail.com>
2020-04-21 20:45:59 +0800

    Add birthdays today funciton

--- program.py
+++ program.py
@@ -6,20 +6,22 @@ db = [
 "birthday": "August 05, 1989"
 }, 
 
-{"id": 2, "first_name": "Adriana", 
+{"id": 2, 
+"first_name": "Adriana", 
 "last_name": "Boston Terrier", 
 "birthday": "August 17, 2017"
 }, 
 
-{"id": 3, "first_name": "Anastasia", 
+{"id": 3, 
+"first_name": "Anastasia", 
 "last_name": "Boston Terrier", 
-"birthday": "August 03, 2017"
+"birthday": "April 22, 2000"
 }, 
 
 {"id": 4, 
 "first_name": "Juan", 
 "last_name": "Dela Cruz", 
-"birthday": "August 30, 2000"
+"birthday": "April 22, 2000"
 }
 
 ]
@@ -33,13 +35,13 @@ def main_menu():
 	if menu_input == 1:
 		show_all()
 	elif menu_input == 2:
-		pass
+		birthdays_today()
 	elif menu_input == 3:
 		pass
 	elif menu_input == 4:
 		pass
 	elif menu_input == 5:
-		pass
+		exit()
 	else:
 		print('Invalid input')
 
@@ -59,6 +61,28 @@ def show_all():
 			
 			''')
 
+	input('Press any key to continue...')
+
+	main_menu()
+
+def search_by_id(user_id):
+	from datetime import datetime
+
+	for item in db:
+		birtdate = datetime.strptime(item['birthday'], '%B %d, %Y')
+		age = calculate_age(birtdate)
+
+		if user_id == item['id']:
+			print(f'''
+
+		ID: {item['id']}
+		Full name: {item['first_name'] + ' ' + item['last_name']}
+		Birthday: {item['birthday']}
+		Age: {age}
+			
+			''')
+
+
 def calculate_age(born):
 	from datetime import date
 
@@ -66,8 +90,25 @@ def calculate_age(born):
 	return today.year - born.year - ((today.month, today.day) < (born.month, born.day))
 
 
+def birthdays_today():
+	from datetime import datetime, date
+	
+	today = date.today()
 
+	with_birthday = False
 
+	for item in db:
+		birtdate = datetime.strptime(item['birthday'], '%B %d, %Y')
+
+		if (today.month, today.day) == (birtdate.month, birtdate.day):
+			with_birthday = True
+			search_by_id(item['id'])
+
+	if with_birthday == False:
+		print('No one has a birthday today.')
+
+	input('Press any key to continue...')
+	main_menu()
 
 main_menu()
 
